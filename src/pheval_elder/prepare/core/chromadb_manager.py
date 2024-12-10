7b6a427ebc75d945fad33e8ef90009ab00325399
iQuxLE <ckroll95@gmail.com>
2024-12-10 17:37:39 +0000

    rework using property for easier access

--- src/pheval_elder/prepare/core/chromadb_manager.py
+++ src/pheval_elder/prepare/core/chromadb_manager.py
@@ -1,7 +1,9 @@
 import logging
+from collections.abc import Sequence
+from functools import lru_cache
 from typing import Optional, Dict
 
-from chromadb import ClientAPI as API
+from chromadb import ClientAPI as API, ClientAPI
 import chromadb
 from dataclasses import dataclass, field
 from chromadb.api.models.Collection import Collection
@@ -16,39 +18,63 @@ logger = logging.getLogger(__name__)
 
 @dataclass
 class ChromaDBManager:
-    collection_name: str
+    collection_name: str = None
     client: API = None
     path: str = None
     ontology: str = field(default="hp")
-    avg_collection_name: str = field(default="avg")
-    avg_weighted_collection_name: str = field(default="wgt_avg")
+    strategy: str = field(default="avg") # wgt_avg
     model_shorthand: str = field(default="ada")
     ont_hp: Collection = None
     similarity: Optional[SimilarityMeasures] = SimilarityMeasures.COSINE
-    hp_embeddings_collection: Collection = None
-    disease_avg_embeddings_collection: Collection = None
-    disease_weighted_avg_embeddings_collection: Collection = None
 
     def __post_init__(self):
         if self.path is None:
             config = config_loader.load_config()
             self.path = config["chroma_db_path"]
             self.client = chromadb.PersistentClient(path=self.path)
-        if self.ont_hp is None:
+        else:
+            self.client = chromadb.PersistentClient(path=self.path)
+        if self.ont_hp is None and self.collection_name:
             self.ont_hp = self.client.get_collection(self.collection_name)
 
-
-        self.hp_embeddings_collection = self.get_or_create_collection(
-            f"{self.model_shorthand}_{self.ontology}_{self.collection_name}"
-        )
-        self.disease_avg_embeddings_collection = self.get_or_create_collection(
-            f"{self.model_shorthand}_{self.avg_collection_name}_{self.collection_name}"
-        )
-        self.disease_weighted_avg_embeddings_collection = self.get_or_create_collection(
-            f"{self.model_shorthand}_{self.avg_weighted_collection_name}_{self.collection_name}"
+    @property
+    def disease_weighted_avg_embeddings_collection(self) -> Collection:
+        return self._get_disease_weighted_avg_embeddings_collection(
+            self.collection_name,
+            self.strategy,
+            self.model_shorthand
         )
 
-    def get_or_create_collection(self, name: str):
+    @property
+    def disease_avg_embeddings_collection(self) -> Collection:
+        return self._get_disease_avg_embeddings_collection(
+            self.collection_name,
+            self.strategy,
+            self.model_shorthand
+        )
+
+    def _get_disease_avg_embeddings_collection(
+            self,
+            collection_name: str,
+            avg_strategy: str,
+            model_shorthand: str
+    ) -> Collection:
+        avg_collection = self.get_or_create_collection(
+            name=f"{model_shorthand}_{avg_strategy}_{collection_name}",
+        )
+        return avg_collection
+
+    def _get_disease_weighted_avg_embeddings_collection(
+            self,
+            collection_name: str,
+            avg_weighted_strategy: str,
+            model_shorthand: str
+    ) -> Collection:
+        avg_wgt_collection = self.get_or_create_collection(
+            name=f"{model_shorthand}_{avg_weighted_strategy}_{collection_name}"        )
+        return avg_wgt_collection
+
+    def get_or_create_collection(self, name: str) -> Collection:
         try:
             similarity_str_value = self.similarity.value if self.similarity else SimilarityMeasures.COSINE.value
             collection = self.client.get_or_create_collection(
@@ -63,5 +89,5 @@ class ChromaDBManager:
         except Exception as e:
             raise ValueError(f"Error getting/creating collection {name}: {str(e)}")
 
-    def list_collections(self):
+    def list_collections(self) -> Sequence[Collection]:
         return self.client.list_collections()
